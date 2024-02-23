407fc1053a7e12298767f01257d7ecf5f7464852
contigen <69436315+contigen@users.noreply.github.com>
2024-02-23 23:18:23 +0100

    Update social links URL

--- components/ui/icons/index.tsx
+++ components/ui/icons/index.tsx
@@ -4,10 +4,13 @@ import { LinkedIn } from "./linkedin"
 import { X } from "./x"
 
 export const ICONS = new Map()
-  .set(Facebook, `https://faceboook.com/gdscui`)
-  .set(Instagram, `https://instagram.com/gdsc_ui`)
+  .set(
+    Facebook,
+    `https://m.facebook.com/people/Developer-Student-Clubs-UI/100066824497698/`
+  )
+  .set(Instagram, `https://www.instagram.com/gdsc_ui`)
   .set(
     LinkedIn,
     `https://www.linkedin.com/company/developer-students-club-university-of-ibadan`
   )
-  .set(X, `https://x.com/gdscui`)
+  .set(X, `https://x.com/gdsc_ui`)
