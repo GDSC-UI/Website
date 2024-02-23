cefcf77151e0f96c4da1d6c5a2b879e81534b3ce
contigen <69436315+contigen@users.noreply.github.com>
2024-02-23 22:54:18 +0100

    Add hero image component

---
+++ components/ui/hero-image.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import heroSrc from "public/gdsc-hero.jpg"
+
+export function HeroImage() {
+  return (
+    <div className="lg:mx-[7.5rem] mt-20">
+      <Image
+        src={heroSrc}
+        alt="hero image with GDSC UI as text in a white background box on a yellow patterned background"
+        placeholder="blur"
+      />
+    </div>
+  )
+}
