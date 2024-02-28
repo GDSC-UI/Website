57945c8626dc4807d52d8031ee2aa55bbc3950b5
contigen <69436315+contigen@users.noreply.github.com>
2024-02-28 11:04:52 +0100

    Add rotate  image component

--- components/ui/flex-container.tsx
+++ components/ui/flex-container.tsx
@@ -1,10 +1,6 @@
-import {
-  ComponentPropsWithRef,
-  PropsWithChildren,
-  PropsWithRef,
-  ReactNode,
-} from "react"
+import { ComponentPropsWithoutRef, PropsWithChildren, ReactNode } from "react"
 import Button from "./Button"
+import { RotatedImage } from "./rotated-image"
 
 export function FlexContainer({
   heading,
@@ -15,25 +11,20 @@ export function FlexContainer({
   period: string
 }) {
   return (
-    <div className="flex gap-16 bg-gray-100 px-[7.5rem] items-center justify-center">
+    <div className="flex gap-16 bg-lightblue px-[7.5rem] items-center justify-center">
       <div className="space-y-5 py-[7.5rem] basis-1/2">
         <h1 className="capitalize">{heading}</h1>
-        <span className="font-bold inline-block">{period}</span>
-        <p>
-          Lorem ipsum dolor sit amet consectetur. Egestas turpis integer et sit
-          enim nibh ac neque viverra. Scelerisque neque elit tempus metus
-          interdum luctus eget eu. Senectus lorem nec habitasse libero senectus
-          proin velit. Dis aliquam convallis donec adipiscing pulvinar enim duis
-          fermentum volutpat.
-        </p>
+        <span className="inline-block font-bold">{period}</span>
         {children}
       </div>
-      <div className="size-[400px] bg-gray-200 border border-blue"></div>
+      <div className="isolate">
+        <RotatedImage />
+      </div>
     </div>
   )
 }
 
-type FlexContainerProps = ComponentPropsWithRef<typeof FlexContainer>
+type FlexContainerProps = ComponentPropsWithoutRef<typeof FlexContainer>
 
 export const FlexContainerWithButton = ({
   children,
