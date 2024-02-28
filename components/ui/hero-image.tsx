fdf83a41aac367effc53d74768eb97c4b929c653
contigen <69436315+contigen@users.noreply.github.com>
2024-02-28 15:14:20 +0100

    Add margin-bottom

--- components/ui/hero-image.tsx
+++ components/ui/hero-image.tsx
@@ -3,7 +3,7 @@ import heroSrc from "public/gdsc-hero.jpg"
 
 export function HeroImage() {
   return (
-    <div className="lg:mx-[7.5rem] mt-20">
+    <div className="lg:mx-[7.5rem] my-20">
       <Image
         src={heroSrc}
         alt="hero image with GDSC UI as text in a white background box on a yellow patterned background"
