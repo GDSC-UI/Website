cd6ec455630542e7215bc9fb6036a3ec242b36fd
contigen <69436315+contigen@users.noreply.github.com>
2024-02-28 15:15:54 +0100

    Uncomment speakers list code
    The current implementation  is without the appropriate props, and as expected, errors out

--- components/homepage/organizers/index.tsx
+++ components/homepage/organizers/index.tsx
@@ -1,27 +1,28 @@
-import SpeakerCard from "@/components/shared/speaker";
+import SpeakerCard from "@/components/shared/speaker"
 
 const Organizers = () => {
-  return (     <section className=" px-4 md:px-28 md:py-20 py-16  bg-lightblue">
+  return (
+    <section className="px-4 py-16  md:px-28 md:py-20 bg-lightblue">
+      <h3 className="text-3xl font-bold text-center  md:text-5xl">
+        Meet Our Organizers
+      </h3>
+      <p className="max-w-2xl mx-auto mt-6 font-light text-center">
+        Meet our chefs. Get to know the people behind the magic; the brains
+        behind the scene.
+      </p>
 
-  <h3 className=" font-bold md:text-5xl text-3xl text-center">
-  Meet Our Organizers
-  </h3>
-  <p className="text-center  font-light max-w-2xl mx-auto mt-6">
-  Meet our chefs. Get to know the people behind the magic; the brains behind the scene.
-  </p>
-
-  <div className="flex mt-14 gap-16 flex-wrap justify-center">
-    <SpeakerCard/>
-    <SpeakerCard/>
-    <SpeakerCard/>
-    <SpeakerCard/>
-    <SpeakerCard/>
-    <SpeakerCard/>
-    <SpeakerCard/>
-    <SpeakerCard/>
-  </div>
-  
-  </section> );
+      <div className="flex flex-wrap justify-center gap-16 mt-14">
+        {/* <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard /> */}
+      </div>
+    </section>
+  )
 }
- 
-export default Organizers;
+
+export default Organizers
