8cf776f85056c05d5fc8275bf0e3ebdb73183f40
contigen <69436315+contigen@users.noreply.github.com>
2024-02-28 11:05:32 +0100

    Update speakers card

--- components/shared/speaker/index.tsx
+++ components/shared/speaker/index.tsx
@@ -1,9 +1,21 @@
-const SpeakerCard = () => {
-  return ( <div className="text-center p-5">
-    <div className="bg-lightgrey rounded-[50%] w-36 h-36 mx-auto"></div>
-    <h4 className="font-bold  pt-4">Michael Segun</h4>
-    <p className="font-light ">Technical Design Lead</p>
-  </div> );
+import Image, { StaticImageData } from "next/image"
+
+const SpeakerCard = ({ name, src }: { name: string; src: StaticImageData }) => {
+  return (
+    <div className="text-center p-5">
+      <div className="bg-lightgrey rounded-[50%] w-36 h-36 mx-auto">
+        <Image
+          src={src}
+          alt={`${name}ʼs cover photo`}
+          quality={100}
+          placeholder="blur"
+          className="rounded-[7.5rem]"
+        />
+      </div>
+      <h4 className="font-bold  pt-4">{name}</h4>
+      <p className="font-light ">Software Engineer</p>
+    </div>
+  )
 }
- 
-export default SpeakerCard;
+
+export default SpeakerCard
