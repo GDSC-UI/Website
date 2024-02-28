95be058c5d2fae766f9b65c003cccd15a994a40d
contigen <69436315+contigen@users.noreply.github.com>
2024-02-28 15:16:59 +0100

    Disable eslint  alt text warning

--- components/ui/image.tsx
+++ components/ui/image.tsx
@@ -4,5 +4,6 @@ import { ComponentPropsWithoutRef } from "react"
 type ImageProps = ComponentPropsWithoutRef<typeof Image>
 
 export const MyImage = (props: ImageProps) => (
+  // eslint-disable-next-line jsx-a11y/alt-text
   <Image {...props} quality={100} placeholder="blur" />
 )
