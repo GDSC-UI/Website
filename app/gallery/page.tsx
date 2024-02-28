884b547afc00b06460a4aa437119bc646dc15608
contigen <69436315+contigen@users.noreply.github.com>
2024-02-28 11:02:50 +0100

    Update gallery  page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,16 +1,11 @@
 import Button from "@/components/ui/Button"
-import { TwobyTwoGrid } from "@/components/ui/grid-container"
+import { Gallery } from "@/components/ui/gallery"
 import { HeroImage } from "@/components/ui/hero-image"
 import { Section } from "@/components/ui/section"
-import Image from "next/image"
-import Image1 from "./images/image_1.jpg"
-import Image2 from "./images/image_2.jpg"
-import Image3 from "./images/image_3.jpg"
-import Image4 from "./images/image_4.jpg"
-import Image5 from "./images/image_5.jpg"
-import Image6 from "./images/image_6.jpg"
 
-const IMAGES = [Image1, Image2, Image3, Image4, Image5, Image6]
+export const metadata = {
+  title: `Gallery`,
+}
 
 export default function Page() {
   return (
@@ -23,28 +18,12 @@ export default function Page() {
           interdum luctus eget eu.
         </p>
       </Section>
-      <section className="mx-[7.5rem] space-y-10 mb-20">
+      <section className="mx-[7.5rem] space-y-10 mber-20">
         <Gallery />
         <div className="text-center">
           <Button variant="outline">See more photos</Button>
         </div>
       </section>
     </div>
-  )
-}
-
-async function Gallery() {
-  return (
-    <TwobyTwoGrid>
-      {IMAGES.map((image) => (
-        <Image
-          key={crypto.randomUUID()}
-          src={image}
-          alt="Photo of attendees during a GDSC UI program"
-          placeholder="blur"
-          quality={100}
-        />
-      ))}
-    </TwobyTwoGrid>
   )
 }
