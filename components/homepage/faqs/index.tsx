b905e2aa8a256365fee6674b77aa91c66dfc9c6c
Blazing-mike <adebambomichael5@gmail.com>
2024-03-06 14:29:25 +0100

    update FAQ

--- components/homepage/faqs/index.tsx
+++ components/homepage/faqs/index.tsx
@@ -3,7 +3,7 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
+} from "@/components/ui/accordion"
 
 const FAQS = () => {
   return (
@@ -18,33 +18,60 @@ const FAQS = () => {
       <div className="md:w-8/12 mx-auto mt-10">
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger className="">Who is GDSC?</AccordionTrigger>
-            <AccordionContent className="font-medium">
-              Who is GDSC?
+            <AccordionTrigger className="">What is GDSC?</AccordionTrigger>
+            <AccordionContent>
+              GDSC stands for Google Developer Student Clubs, which are
+              community groups for students interested in Google developer
+              technologies. These clubs provide opportunities to learn, share
+              knowledge, and engage in hands-on projects.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger className="">Question 1</AccordionTrigger>
-            <AccordionContent className="font-medium">
-              Who is GDSC?
+            <AccordionTrigger className="">Who can join GDSC?</AccordionTrigger>
+            <AccordionContent className="">
+              Any student passionate about technology, regardless of their major
+              or background, can join GDSC. Whether you`&apos;`re a beginner or
+              an experienced developer, GDSC welcomes everyone who wants to
+              learn and contribute.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
-            <AccordionTrigger className="">Question 1</AccordionTrigger>
-            <AccordionContent className="font-medium">
-              Who is GDSC?
+            <AccordionTrigger className="">
+              How can I get involved with GDSC?
+            </AccordionTrigger>
+            <AccordionContent className="">
+              To get involved, simply attend our events, join our community
+              discussions, and participate in our projects. You can also become
+              a GDSC member or apply to be part of the leadership team when
+              opportunities arise.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-4">
-            <AccordionTrigger className="">Question 1</AccordionTrigger>
+            <AccordionTrigger className="">
+              Are there any membership requirements for GDSC?
+            </AccordionTrigger>
             <AccordionContent className="font-medium">
-              Who is GDSC?
+              GDSC membership is open to all students and there are no specific
+              requirements to join. We encourage active participation and
+              enthusiasm for learning and sharing knowledge.
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="item-5">
+            <AccordionTrigger className="">
+              How can I stay updated about GDSC events and activities?
+            </AccordionTrigger>
+            <AccordionContent className="font-medium">
+              To stay updated, follow us on social media platforms such as
+              Twitter, Facebook, and Instagram, and join our mailing list. You
+              can also check our website regularly for event announcements and
+              updates.
             </AccordionContent>
           </AccordionItem>
         </Accordion>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default FAQS;
+export default FAQS
